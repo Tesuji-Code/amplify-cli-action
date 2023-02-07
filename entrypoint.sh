95abf73b1df7bdc62c83853907b9c7968acea95d
Dexter <Apogee-DK@users.noreply.github.com>
2023-02-06 23:57:36 -0800

    Update entrypoint.sh
    Remove unnecessary commands, and use import feature instead.

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,6 +7,11 @@ if [ -z "$AWS_ACCESS_KEY_ID" ] && [ -z "$AWS_SECRET_ACCESS_KEY" ] ; then
   exit 1
 fi
 
+if [ -z "$AUTHCONFIG" ] ; then
+  echo "You must provide AUTHCONFIG environment variables in order to deploy"
+  exit 1
+fi
+
 if [ -z "$AWS_REGION" ] ; then
   echo "You must provide AWS_REGION environment variable in order to deploy"
   exit 1
@@ -45,81 +50,13 @@ which amplify
 echo "amplify version $(amplify --version)"
 
 case $5 in
-
-  push)
-    amplify push $9 --yes
-    ;;
-
-  publish)
-    amplify publish $9 --yes
-    ;;
-
-  status)
-    amplify status $9
-    ;;
-
-  configure)
+  import)
     aws_config_file_path="$(pwd)/aws_config_file_path.json"
     echo '{"accessKeyId":"'$AWS_ACCESS_KEY_ID'","secretAccessKey":"'$AWS_SECRET_ACCESS_KEY'","region":"'$AWS_REGION'"}' > $aws_config_file_path
     echo '{"projectPath": "'"$(pwd)"'","defaultEditor":"code","envName":"'$6'"}' > ./amplify/.config/local-env-info.json
     echo '{"'$6'":{"configLevel":"project","useProfile":false,"awsConfigFilePath":"'$aws_config_file_path'"}}' > ./amplify/.config/local-aws-info.json
-
-
-
-    # if environment doesn't exist fail explicitly
-    if [ -z "$(amplify env get --name $6 | grep 'No environment found')" ] ; then
-      echo "found existing environment $6"
-      amplify env pull --yes $9
-    else
-      echo "$6 environment does not exist, consider using add_env command instead";
-      exit 1
-    fi
+    echo $AUTHCONFIG | jq -c | amplify import auth --headless
 
     amplify status
-    ;;
-
-  add_env)
-    AMPLIFY="{\
-    \"envName\":\"$6\"\
-    }"
-
-    AWSCLOUDFORMATIONCONFIG="{\
-    \"configLevel\":\"project\",\
-    \"useProfile\":false,\
-    \"accessKeyId\":\"$AWS_ACCESS_KEY_ID\",\
-    \"secretAccessKey\":\"$AWS_SECRET_ACCESS_KEY\",\
-    \"region\":\"$AWS_REGION\"\
-    }"
-
-    PROVIDERS="{\
-    \"awscloudformation\":$AWSCLOUDFORMATIONCONFIG\
-    }"
-
-    amplify env add $9 --amplify "$AMPLIFY" --providers "$PROVIDERS" --yes
-    amplify status
-    ;;
-
-  delete_env)
-    # ACCIDENTAL DELETION PROTECTION #0: delete_lock
-    if [ "$7" = true ] ; then
-      echo "ACCIDENTAL DELETION PROTECTION: You must unset delete_lock input parameter for delete to work"
-      exit 1
-    fi
-
-    # ACCIDENTAL DELETION PROTECTION #1: environment to be deleted cannot contain prod/release/master in its name
-    if [[ ${6,,} =~ prod|release|master ]] ; then
-      echo "ACCIDENTAL DELETION PROTECTION: delete command is unsupported for environments that contain prod/release/master in its name"
-      exit 1
-    fi
-
-    # fill in dummy env in local-env-info so we delete current environment
-    # without switch to another one (amplify restriction)
-    echo '{"projectPath": "'"$(pwd)"'","defaultEditor":"code","envName":"dummyenvfordeletecurrentowork"}' > ./amplify/.config/local-env-info.json
-    echo "Y" | amplify env remove "$6" $9
-    ;;
-
-  *)
-    echo "amplify command $5 is invalid or not supported"
-    exit 1
     ;;
 esac
