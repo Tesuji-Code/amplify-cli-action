345ac2a745d2487b8f610c36d16f9b6a143d363f
Dexter <Apogee-DK@users.noreply.github.com>
2023-02-07 21:58:29 -0800

    Added Quotations Around Parameters

--- entrypoint.sh
+++ entrypoint.sh
@@ -51,13 +51,26 @@ case $5 in
     echo '{"accessKeyId":"'$AWS_ACCESS_KEY_ID'","secretAccessKey":"'$AWS_SECRET_ACCESS_KEY'","region":"'$AWS_REGION'"}' > $aws_config_file_path
     echo '{"projectPath": "'"$(pwd)"'","defaultEditor":"code","envName":"'$6'"}' > ./amplify/.config/local-env-info.json
     echo '{"'$6'":{"configLevel":"project","useProfile":false,"awsConfigFilePath":"'$aws_config_file_path'"}}' > ./amplify/.config/local-aws-info.json
-    echo '{"auth":{"userPoolId":"'$AWS_USER_POOL_ID'","webClientId":"'$AWS_WEB_CLIENT_ID'","nativeClientId":"'$AWS_NATIVE_CLIENT_ID'","identityPoolId":"'$AWS_IDENTITY_POOL'","facebookAppIdUserPool":"'$FACEBOOK_APP_ID'","facebookAppSecretUserPool":"'$FACEBOOK_SECRET'","googleAppIdUserPool":"'$GOOGLE_APP_ID'","googleAppSecretUserPool":"'$GOOGLE_SECRET'"}}'  > ./amplify/.config/auth.json
     
+    AUTHCONFIG="{\
+    \"userPoolId\":\"$AWS_USER_POOL_ID\",\
+    \"webClientId\":\"$AWS_WEB_CLIENT_ID\",\
+    \"nativeClientId\":\"$AWS_NATIVE_CLIENT_ID\",\
+    \"identityPoolId\":\"$AWS_IDENTITY_POOL\",\
+    \"facebookAppIdUserPool\":\"$FACEBOOK_APP_ID\",\
+    \"facebookAppSecretUserPool\":\"$FACEBOOK_SECRET\",\
+    \"googleAppIdUserPool\":\"$GOOGLE_APP_ID\",\
+    \"googleAppSecretUserPool\":\"$GOOGLE_SECRET\"\
+    }"
+
+    CATEGORIES="{\
+    \"auth\":$AUTHCONFIG\
+    }"
     
     # if environment doesn't exist fail explicitly
     if [ -z "$(amplify env get --name $6 | grep 'No environment found')" ] ; then
       echo "found existing environment $6"
-      amplify env pull --categories ./amplify/.config/auth.json --yes
+      amplify env pull --categories "$CATEGORIES" --yes
     else
       echo "$6 environment does not exist, consider using add_env command instead";
       exit 1
